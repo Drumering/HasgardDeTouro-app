b6b5bf515dc71103cdcd1c46ac10f1381cb6ff5f
Anderson Mendes de Almeida <ama@cesar.org.br>
2020-07-03 00:35:33 -0300

    [TASK]Updated mattress detail activty, home activity and added new icons

--- app/src/main/java/com/opet/hasgarddetouro_app/activities/DetailMattressActivity.java
+++ app/src/main/java/com/opet/hasgarddetouro_app/activities/DetailMattressActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.opet.hasgarddetouro_app.R;
 import com.opet.hasgarddetouro_app.objects.Matress;
+import com.squareup.picasso.Picasso;
 
 public class DetailMattressActivity extends AppCompatActivity {
 
     private Matress matress;
+    private ImageView imageMatress;
+    private TextView conditions, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,17 @@ public class DetailMattressActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_mattress);
 
         matress = (Matress) getIntent().getSerializableExtra("obj");
-        Log.d("OBJETO", matress.getName());
+
+        imageMatress = findViewById(R.id.mattressImage);
+        conditions = findViewById(R.id.textConditions);
+        description = findViewById(R.id.textDescription);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Picasso.get().load(matress.getImages().get(0)).into(imageMatress);
+        conditions.setText("R$ " + matress.getCashPrice().toString() + " à vista");
+        description.setText(matress.getDescription());
     }
 }
